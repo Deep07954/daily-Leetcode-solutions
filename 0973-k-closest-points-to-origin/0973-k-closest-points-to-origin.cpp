7afc092db27644eec09a081a8b5da2054fedddcb
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-03-22 20:08:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
-static bool comp(vector<int>&a,vector<int>&b){
-            int d1=a[0]*a[0]+a[1]*a[1];
-            int d2=b[0]*b[0]+b[1]*b[1];
-            return d1<d2;
-        };
+// static bool comp(vector<int>&a,vector<int>&b){
+//             int d1=a[0]*a[0]+a[1]*a[1];
+//             int d2=b[0]*b[0]+b[1]*b[1];
+//             return d1<d2;
+//         };
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+
         //t.c O(N) AVG  AND WORST TIME : O(N^2);
         //APPROACH 1
         // auto comp=[&](vector<int>&a,vector<int>&b){
@@ -16,7 +17,21 @@ static bool comp(vector<int>&a,vector<int>&b){
 // SECOND WAY OF USING COMP  USIGN STATIC 
 
 
-        nth_element(points.begin(),points.begin()+k,points.end(),comp);
-        return vector<vector<int>>(points.begin(),points.begin()+k);
+        // nth_element(points.begin(),points.begin()+k,points.end(),comp);
+        // return vector<vector<int>>(points.begin(),points.begin()+k);
+//approach 2 using max heap
+priority_queue<pair<int,int>> pq;
+for(int i=0;i<points.size();i++){
+    int d1=points[i][0]*points[i][0]+points[i][1]*points[i][1];
+    pq.push({d1,i});
+    if(pq.size()>k){
+    pq.pop();}
+}
+vector<vector<int>>ans;
+while(!pq.empty()){
+    ans.push_back(points[pq.top().second]);
+    pq.pop();
+}
+return ans;
     }
 };
