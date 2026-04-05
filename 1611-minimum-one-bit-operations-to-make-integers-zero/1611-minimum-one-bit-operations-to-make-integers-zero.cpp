bc55612d2da5cb0a1db11115efa279eecb8bd35f
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-04-05 14:57:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.cpp
+++ 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
     int minimumOneBitOperations(int n) {
-  vector<int>F(31,0);
-  F[0]=1;
-  for(int i=1;i<=30;i++){
-    F[i]=2*F[i-1]+1;//ye jab ith bit sirf se ho eg 1000 10000
-  }
-  int result=0;
-  int sign=1; // ek baar + hoga ek baar minus alterante
-  //left se right jayenge
-  for(int i=30;i>=0;i--){
-    int ith_bit=(1<<i)&n;
-    if(ith_bit==0)
-    continue;
-if(sign>0){
-    result+=F[i];
-}
-else
-result-=F[i]; 
+//   vector<int>F(31,0);
+//   F[0]=1;
+//   for(int i=1;i<=30;i++){
+//     F[i]=2*F[i-1]+1;//ye jab ith bit sirf se ho eg 1000 10000
+//   }
+//   int result=0;
+//   int sign=1; // ek baar + hoga ek baar minus alterante
+//   //left se right jayenge
+//   for(int i=30;i>=0;i--){
+//     int ith_bit=(1<<i)&n;
+//     if(ith_bit==0)
+//     continue;
+// if(sign>0){
+//     result+=F[i];
+// }
+// else
+// result-=F[i]; 
 
-sign=sign*(-1);
+// sign=sign*(-1);
 
-      }
-        return result;
-    }
-};
+//       }
+//         return result;
+//     }
+// }; //appraoch -2 gery vode to binary 
+   int res = 0;
+        // This loop implements the Gray-to-Binary conversion
+        // which maps the Gray code 'n' back to its original integer index.
+        while (n > 0) {
+            res ^= n;
+            n >>= 1;
+        }
+        return res;
+        }
+        };
