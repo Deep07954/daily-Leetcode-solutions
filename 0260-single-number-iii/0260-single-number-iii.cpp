3180c9cd442fcc4de5e0c02fc9b04f4b0711cbda
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-04-06 08:26:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -4,16 +4,33 @@ public:
         int n=nums.size();
         if(n==2)
         return nums;
-        unordered_map<int,int>mp;
+        // unordered_map<int,int>mp;
+        // for(auto x:nums){
+        //     mp[x]++;
+        // }
+        // vector<int>result;
+        // for(auto p:mp){
+        //     if(p.second<=1){
+        //         result.push_back(p.first);
+        //     }
+        // }
+        // return result;
+        //TIME COMPELXITY : O(n)
+        //space complexity : o(n)
+        int xorall=0;
         for(auto x:nums){
-            mp[x]++;
+            xorall^=x;
         }
-        vector<int>result;
-        for(auto p:mp){
-            if(p.second<=1){
-                result.push_back(p.first);
+        int b1=0;
+        int b2=0;
+        int rightmost_bit=(xorall&(xorall-1))^xorall;
+        for(auto x:nums){
+            if(x&rightmost_bit){
+                b1=b1^x;
             }
+            else
+            b2=b2^x;
         }
-        return result;
+        return {b1,b2};
     }
 };
