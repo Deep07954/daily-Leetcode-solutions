0786e9828005c8b25971ddf70724c0767768ca93
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-04-11 11:08:15 +0530

    Time: 4 ms (18.61%), Space: 11.2 MB (80.09%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
-              while(right>left){
-            right=right&(right-1);
-        }
-     return right;
+      //approach-2
+    int shift=0;
+    while(left!=right){
+        left>>=1;
+        right>>=1;
+        shift++;
+    }
+    return right<<shift;
+
+    //           while(right>left){
+    //         right=right&(right-1);
+    //     }
+    //  return right;
     }
 };
