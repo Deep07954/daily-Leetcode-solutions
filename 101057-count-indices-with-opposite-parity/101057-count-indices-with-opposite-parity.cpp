bd27ac483c55f45d4aef3276649e02935e432edd
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-05-03 08:25:56 +0530

    Time: 8 ms (0%), Space: 32.9 MB (14.29%) - LeetHub

---
+++ 101057-count-indices-with-opposite-parity/101057-count-indices-with-opposite-parity.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> countOppositeParity(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)
+            return {0};
+        vector<int>result;
+
+          
+        for(int i=0;i<n;i++){
+              int count=0;
+            for(int j=i+1;j<n;j++){
+                if((nums[i]%2)!=0){
+                    if(nums[j]%2==0){
+                        count++;
+                    }
+                }
+                else
+                    if((nums[i]%2)==0){
+                        if(nums[j]%2!=0){
+                        count++;
+                    }
+                    }
+            }
+            result.push_back(count);
+        }
+        return result;
+    }
+};
