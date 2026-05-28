33cd9a4ae267f849df9a79aa42fd85c4e346662b
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-05-28 13:59:43 +0530

    Time: 851 ms (9.55%), Space: 9 MB (75.79%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+int result;
+void solve(int i,vector<int>&ans,vector<int>&cookies,int k){
+    int n=cookies.size();
+    if(i>=n||i<0){
+        int maxm=*max_element(begin(ans),end(ans));
+        result=min(maxm,result);
+        return;
+    }
+   
+    for(int j=0;j<k;j++){
+        ans[j]+=cookies[i];
+        solve(i+1,ans,cookies,k);
+        ans[j]-=cookies[i];
+
+    }
+}
+    int distributeCookies(vector<int>& cookies, int k) {
+        result=INT_MAX;
+        vector<int>ans(k,0);
+        solve(0,ans,cookies,k);
+        return result;
+    }
+};
