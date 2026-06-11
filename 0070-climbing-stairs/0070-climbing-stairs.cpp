2a18535a5db741e474f3640a5743b5c437155edc
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-06-12 00:23:04 +0530

    Time: 0 ms (100%), Space: 7.8 MB (70.52%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
-int dp[46];
-int solve(int n){
-    if(n<0)
-    return 0;
-    if(n==0)
-    return 1;
-    if(dp[n]!=-1)
-    return dp[n];
+// int dp[46];
+// int solve(int n){
+//     if(n<0)
+//     return 0;
+//     if(n==0)
+//     return 1;
+//     if(dp[n]!=-1)
+//     return dp[n];
 
-    return dp[n]=solve(n-1)+solve(n-2);
-}
+//     return dp[n]=solve(n-1)+solve(n-2);
+// }
     int climbStairs(int n) {
        //top down approach
-       memset(dp,-1,sizeof(dp));
-      int result=solve(n);
-      return result;
+    //    memset(dp,-1,sizeof(dp));
+    //   int result=solve(n);
+    //   return result;
+    //bottom up
+    int dp[46];
+    dp[1]=1;
+    dp[2]=2;
+    dp[3]=3;
+    for(int i=3;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+
     }
+    return dp[n];
+        }
 };
