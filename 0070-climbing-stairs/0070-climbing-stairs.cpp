3bea1569f56c4cc6536080fcc04fb482783f1b6c
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-06-12 00:50:34 +0530

    Time: 0 ms (100%), Space: 7.9 MB (70.52%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -17,14 +17,30 @@ public:
     //   int result=solve(n);
     //   return result;
     //bottom up
-    int dp[46];
-    dp[1]=1;
-    dp[2]=2;
-    dp[3]=3;
-    for(int i=3;i<=n;i++){
-        dp[i]=dp[i-1]+dp[i-2];
+    // int dp[46];
+    // dp[1]=1;
+    // dp[2]=2;
+    // dp[3]=3;
+    // for(int i=3;i<=n;i++){
+    //     dp[i]=dp[i-1]+dp[i-2];
 
-    }
-    return dp[n];
+    // }
+    // return dp[n];
+    //space optimization
+   
+int a=1;
+int b=2;
+int c=0;
+ if(n<=1)
+    return a;
+    if(n==2)
+    return b;
+for(int i=3;i<=n;i++){
+    c=a+b;
+    a=b;
+    b=c;
+}
+return c;
+
         }
 };
