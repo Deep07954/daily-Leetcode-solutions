a3d467ab1dfcfa47f0e77e2628d238bbe9fea560
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-06-17 18:19:18 +0530

    Time: 103 ms (5.03%), Space: 174.1 MB (85.01%) - LeetHub

---
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+int dp[1000001];
+    int climbStairs(int n, vector<int>& costs) {
+    dp[0]=0;
+   
+    
+    for(int i=1;i<=n;i++){
+        dp[i]=INT_MAX;
+if(i-1>=0){
+    dp[i]=min(dp[i],costs[i-1]+dp[i-1]+1);
+
+}    
+if(i-2>=0){
+        dp[i]=min(dp[i],costs[i-1]+dp[i-2]+4);
+}
+if(i-3>=0){
+    dp[i]=min(dp[i],costs[i-1]+dp[i-3]+9);
+}
+    }
+    return dp[n];
+
+    }
+};
