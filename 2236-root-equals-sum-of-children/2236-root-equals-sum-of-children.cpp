b5f5cec910c4aa11697b393eb5f99bb0bd13bb06
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-06-17 14:19:07 +0530

    Time: 0 ms (100%), Space: 15 MB (92.14%) - LeetHub

--- 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
@@ -11,12 +11,26 @@
  */
 class Solution {
 public:
-
+int dfs(TreeNode* root){
+    if(root==NULL)
+    return 0;
+     int leftsum=dfs(root->left);
+     int rightsum=dfs(root->right);
+     return root->val+leftsum+rightsum;
+     // directly just write 
+     // return root->val+dfs(root-.left)+dfs(root->right);
+}
     bool checkTree(TreeNode* root) {
-        if(root==NULL)
-        return false;
+    //     if(root==NULL)
+    //     return false;
      
-       return  root->val==root->left->val+root->right->val;
-    
+    //    return  root->val==root->left->val+root->right->val;
+    //approach-2 for nore than 3 ndoes
+    if(root==NULL)
+    return false;
+    int leftsum=dfs(root->left);
+    int rightsum=dfs(root->right);
+    return root->val==leftsum+rightsum;
     }
 };
+// general case ke liye code likhe hua to 
