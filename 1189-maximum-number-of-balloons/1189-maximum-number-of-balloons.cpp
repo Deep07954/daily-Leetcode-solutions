fdba1dbced38eea03c058ef929bd623766654025
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-06-22 14:04:48 +0530

    Time: 3 ms (12.33%), Space: 8.9 MB (64.87%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        
+        unordered_map<char,int>mp;
+        for(auto &c:text){
+            mp[c]++;
+        }
+        int b=mp['b'];
+        int a=mp['a'];
+        int l=mp['l']/2;
+        int o=mp['o']/2;
+        int n=mp['n'];
+        return min({b,a,l,o,n});
+    }
+};
