42f389bdaa1feaa7f872bc98dbef828273810e19
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-07-12 12:53:27 +0530

    Time: 73 ms (50.76%), Space: 14.3 MB (70.99%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,27 +1,40 @@
 class Solution {
 public:
 int n;
-int dp[2501][2501];
-int solve(vector<int>&nums,int i,int p){
-    if(i>=nums.size()){
-        return 0;
-    }
-    if(p!=-1 && dp[i][p]!=-1){
-        return dp[i][p];
-    }
-    int take=0;
-    if(p==-1||nums[p]<nums[i]){
-        take=1+solve(nums,i+1,i);
-    }
-    int skip=solve(nums,i+1,p);
-    if(p!=-1){
-     dp[i][p]=max(take,skip);}
-     return max(take,skip);
-}
+// int dp[2501][2501];
+// int solve(vector<int>&nums,int i,int p){
+//     if(i>=nums.size()){
+//         return 0;
+//     }
+//     if(p!=-1 && dp[i][p]!=-1){
+//         return dp[i][p];
+//     }
+//     int take=0;
+//     if(p==-1||nums[p]<nums[i]){
+//         take=1+solve(nums,i+1,i);
+//     }
+//     int skip=solve(nums,i+1,p);
+//     if(p!=-1){
+//      dp[i][p]=max(take,skip);}
+//      return max(take,skip);
+// }
     int lengthOfLIS(vector<int>& nums) {
  n=nums.size();
- memset(dp,-1,sizeof(dp));
-  return solve(nums,0,-1);
+//  memset(dp,-1,sizeof(dp));
+//   return solve(nums,0,-1);
+//bottom aproach o(n^2)
+vector<int>dp(n,1);
+int maxL=1;
+for(int i=0;i<n;i++){
+    for(int j=0;j<i;j++){
+        if(nums[j]<nums[i]){
+            dp[i]=max(dp[i],dp[j]+1);
+            maxL=max(maxL,dp[i]);
+        }
+    }
+}
+return maxL;
+
     }
 };
 //t.c- O(nlogn)
