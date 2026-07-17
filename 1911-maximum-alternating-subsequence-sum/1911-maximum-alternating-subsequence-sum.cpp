b788f94a4710cc97fccd762b92f94076b829d1de
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-07-18 00:28:18 +0530

    Time: 142 ms (69.94%), Space: 104.5 MB (67.8%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
 long long dp[100001][2];
-long long solve(vector<int>&nums,int i,bool iseven){
-   
+long long solve(vector<int>& nums,int i,bool iseven){
     if(i==nums.size())
-    {return 0;}
-long long take=0;
-if(dp[i][iseven]!=-1){
-    return dp[i][iseven];
-}
-long long skip=solve(nums,i+1,iseven);
+    return 0;
 
-if(iseven){
-    take=nums[i]+solve(nums,i+1,0);
-}else
-{
-    take=-nums[i]+solve(nums,i+1,1);
-}
-return dp[i][iseven]=max(take,skip);
+
+    if(dp[i][iseven]!=-1){
+        return dp[i][iseven];
+    }
+long long take=0;
+    if(iseven){
+take=nums[i]+solve(nums,i+1,0);
+    }
+    else {
+        take=-nums[i]+solve(nums,i+1,1);
+    }
+    long long skip=solve(nums,i+1,iseven);
+
+    return dp[i][iseven]=max(take,skip);
 }
     long long maxAlternatingSum(vector<int>& nums) {
-        int i=0;
         memset(dp,-1,sizeof(dp));
-       long long ans= solve(nums,i,1);
-       return ans;
+     return solve(nums,0,1);
     }
 };
