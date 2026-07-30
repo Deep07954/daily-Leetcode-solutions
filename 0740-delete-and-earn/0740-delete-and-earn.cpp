71e440bb39782fa486aa8311c317d1df42ef5c87
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-07-30 08:46:41 +0530

    Time: 0 ms (100%), Space: 16.1 MB (56.51%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-int dp[10001];
-int solve(vector<int>&points,int i){
+// int dp[10001];
+int solve(vector<int>&points,vector<int>&dp,int i){
     if(i>=points.size())
     return 0;
     if(dp[i]!=-1)
     return dp[i];
     int take=0;
-    take=points[i]+solve(points,i+2);
-    int skip=solve(points,i+1);
+    take=points[i]+solve(points,dp,i+2);
+    int skip=solve(points,dp,i+1);
 
     return dp[i]=max(take,skip);
 }
@@ -16,11 +16,12 @@ int solve(vector<int>&points,int i){
         int n=nums.size();
         int maxm=*max_element(nums.begin(),nums.end());
 vector<int>points(maxm+1,0);
-          memset(dp,-1,sizeof(dp));
+vector<int>dp(maxm+1,-1);
+        //   memset(dp,-1,sizeof(dp));
           //duplciates remvoal
       for(auto x:nums){
         points[x]+=x;
       }
-        return solve(points,0);
+        return solve(points,dp,0);
     }
 };
