4234c61aa92a3c3eb298bf37d35e894d273fea05
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-08-07 07:06:22 +0530

    Time: 0 ms (100%), Space: 14 MB (94.91%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -23,18 +23,28 @@ int n;
 //  memset(dp,-1,sizeof(dp));
 //   return solve(nums,0,-1);
 //bottom aproach o(n^2)
-vector<int>dp(n,1);
-int maxL=1;
+// vector<int>dp(n,1);
+// int maxL=1;
+// for(int i=0;i<n;i++){
+//     for(int j=0;j<i;j++){
+//         if(nums[j]<nums[i]){
+//             dp[i]=max(dp[i],dp[j]+1);
+//             maxL=max(maxL,dp[i]);
+//         }
+//     }
+// }
+// return maxL;
+vector<int>sorted;
 for(int i=0;i<n;i++){
-    for(int j=0;j<i;j++){
-        if(nums[j]<nums[i]){
-            dp[i]=max(dp[i],dp[j]+1);
-            maxL=max(maxL,dp[i]);
-        }
+    auto it=lower_bound(sorted.begin(),sorted.end(),nums[i]);
+    if(it==sorted.end()){
+        sorted.push_back(nums[i]);
+    }else
+    {
+        *it=nums[i];
     }
 }
-return maxL;
-
+return sorted.size();
     }
 };
-//t.c- O(nlogn)
+//t.c- O(nlogn)e
