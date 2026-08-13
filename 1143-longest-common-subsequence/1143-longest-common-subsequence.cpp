6f3d1ebcb60d75849bbabc47811385b330751166
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-08-13 12:59:46 +0530

    Time: 42 ms (28.54%), Space: 17.3 MB (82.61%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+long long dp[1001][1001];
+int m,n;
+long long solve(string &s1,string& s2,int i,int j){
+    if(i>=m||j>=n)
+       { return 0;}
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s1[i]==s2[j]){
+            return dp[i][j]=1+solve(s1,s2,i+1,j+1);
+        }
+ return dp[i][j]=max(solve(s1,s2,i+1,j),solve(s1,s2,i,j+1));
+    
+}
+    int longestCommonSubsequence(string s1, string s2) {
+        m=s1.length();
+        n=s2.length();
+        memset(dp,-1,sizeof(dp));
+       return (int)solve(s1,s2,0,0);
+    }
+};
