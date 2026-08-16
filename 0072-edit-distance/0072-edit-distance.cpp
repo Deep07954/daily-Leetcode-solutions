956b73e25520dd409e073572d7af384d2953996a
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-08-16 13:18:11 +0530

    Time: 47 ms (12.13%), Space: 11.8 MB (85.06%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,28 +2,28 @@ class Solution {
 public:
 int m,n;
 int dp[502][502];
-int solve(string &s1,string &s2,int i,int j){
-if(i==m){
-    return n-j;
+int solve(string &s1,string &s2,int m,int n){
+if(m==0||n==0){
+    return m+n;
 }
-else if(j==n){
-    return m-i;
+
+if(dp[m][n]!=-1){
+    return dp[m][n];
 }
-if(dp[i][j]!=-1){
-    return dp[i][j];
+if(s1[m-1]==s2[n-1]){
+    return dp[m][n]=solve(s1,s2,m-1,n-1);
+}else {
+int ins=1+solve(s1,s2,m,n-1);
+int del=1+solve(s1,s2,m-1,n);
+int rep=1+solve(s1,s2,m-1,n-1);
+return dp[m][n]= min({ins,del,rep});
 }
-if(s1[i]==s2[j]){
-    return dp[i][j]=solve(s1,s2,i+1,j+1);
-}
-int ins=1+solve(s1,s2,i,j+1);
-int del=1+solve(s1,s2,i+1,j);
-int rep=1+solve(s1,s2,i+1,j+1);
-return dp[i][j]= min({ins,del,rep});
+return -1;
 }
     int minDistance(string word1, string word2) {
         m=word1.length();
         n=word2.length();
         memset(dp,-1,sizeof(dp));
-        return solve(word1,word2,0,0);
+        return solve(word1,word2,m,n);
     }
 };
