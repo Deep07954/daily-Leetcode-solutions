13308ea74330c29fca4487b6c91f3b008d715a45
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-08-17 11:29:28 +0530

    Time: 0 ms (100%), Space: 9.4 MB (72.28%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -3,15 +3,26 @@ public:
     string removeOccurrences(string s, string part) {
         int n=s.length();
         int m=part.length();
-        string res="";
-        for(int i=0;i<n;i++){
-            res.push_back(s[i]);
-            if(res.size()>=m){
-                if(res.substr(res.size()-m)==part){
-                    res.erase(res.size()-m);
-                }
-            }
-        }
-        return res;
+//brute force
+while(true){
+int idx=s.find(part);
+if(idx==string::npos){
+    break;
+}
+s.erase(idx,m);
+}
+return s;
+
+        // string res="";
+        // for(int i=0;i<n;i++){
+        //     res.push_back(s[i]);
+        //     if(res.size()>=m){
+        //         if(res.substr(res.size()-m)==part){
+        //             res.erase(res.size()-m);
+        //         }
+        //     }
+        // }
+        // return res;
+        
     }
 };
