92ec06fb2196903495c043c82c0e855f4bfe7268
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-08-17 11:43:10 +0530

    Time: 15 ms (5.49%), Space: 28.4 MB (5.21%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
+bool check(string part,stack<char>&st,int m){
+    stack<char>temp=st;
+    for(int i=m-1;i>=0;i--){
+        if(temp.top()!=part[i]){
+            return false;
+        }
+        temp.pop();
+    }
+    return true;
+}
     string removeOccurrences(string s, string part) {
         int n=s.length();
         int m=part.length();
 //brute force
-while(true){
-int idx=s.find(part);
-if(idx==string::npos){
-    break;
-}
-s.erase(idx,m);
-}
-return s;
+// while(true){
+// int idx=s.find(part);
+// if(idx==string::npos){
+//     break;
+// }
+// s.erase(idx,m);
+// }
+// return s;
 
         // string res="";
         // for(int i=0;i<n;i++){
@@ -23,6 +33,21 @@ return s;
         //     }
         // }
         // return res;
-        
+        stack<char>st;
+        for(int i=0;i<n;i++){
+            st.push(s[i]);
+            if(st.size()>=m && check(part,st,m)==true){
+                for(int j=0;j<m;j++){
+                    st.pop();
+                }
+            }
+        }
+        string res="";
+        while(!st.empty()){
+            res.push_back(st.top());
+            st.pop();
+        }
+        reverse(res.begin(),res.end());
+        return res;
     }
 };
