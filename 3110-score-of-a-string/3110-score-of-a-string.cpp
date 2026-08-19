3cb6d5e95231a5973af5bc9ffeef9922e1b3ac67
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-08-19 11:03:51 +0530

    Time: 0 ms (100%), Space: 8.9 MB (30.59%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int res=0;
+        int diff=0;
+        for(int i=1;i<s.length();i++){
+            res+=abs((s[i]-'0')-(s[i-1]-'0'));
+            
+        }
+        return res;
+    }
+};
