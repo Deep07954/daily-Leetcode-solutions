cdddc1d497e9ed64d1aafa8d9ead8e8514694d90
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-08-25 17:04:02 +0530

    Time: 11 ms (9.55%), Space: 25.1 MB (45.06%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -2,17 +2,24 @@ class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
         int n=nums.size();
-        vector<bool>freq(105,false);
-        for(auto x:nums){
-            freq[x]=true;
+
+        unordered_set<int>st(nums.begin(),nums.end());
+        int curr=k;
+        while(st.count(curr)){
+            curr+=k;
         }
-        int current=k;
-        while(current<=101){
-if(freq[current]==false){
-    return current;
-}
-current+=k;
-        }
-        return current;
+        return curr;
+//         vector<bool>freq(105,false);
+//         for(auto x:nums){
+//             freq[x]=true;
+//         }
+//         int current=k;
+//         while(current<=101){
+// if(freq[current]==false){
+//     return current;
+// }
+// current+=k;
+//         }
+//         return current;
     }
 };
