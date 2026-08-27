3c54776188214cf45d3808581b1f9df7abe83cee
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-08-27 12:18:49 +0530

    Time: 207 ms (5.02%), Space: 206.5 MB (5.01%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+void dfs(int u,unordered_map<int,vector<pair<int,int>>>&adj,vector<bool>&visited,int &result){
+    visited[u]=true;
+    //neighbour jke pass jao
+    for(auto & p:adj[u]){
+int v=p.first;
+int d=p.second;
+result=min(result,d);
+if(!visited[v]){
+    dfs(v,adj,visited,result);
+}
+    }
+}
+    int minScore(int n, vector<vector<int>>& roads) {
+        unordered_map<int,vector<pair<int,int>>>adj;
+        for( auto vec:roads){
+        int u=vec[0];
+        int v=vec[1];
+        int dist=vec[2];
+        adj[u].push_back({v,dist});
+         adj[v].push_back({u,dist});
+        }
+        vector<bool>visited(n,false);
+        int result=INT_MAX;
+        dfs(1,adj,visited,result);
+        return result;
+
+    }
+};
