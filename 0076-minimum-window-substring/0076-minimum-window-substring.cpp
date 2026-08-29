b19608268a5120dbfb95ad74081289cc53c07841
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-08-29 19:01:59 +0530

    Time: 2 ms (80.91%), Space: 11.4 MB (65.2%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,70 +1,33 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        // brute force
-        // int m = s.length();
-        // int n = t.length();
-        // int minlen = INT_MAX;
-        // int sindex = -1;
-        
-        // for (int i = 0; i < m; i++) {
-        //     vector<int> hash(256, 0);
-        // for (auto& c : t) {
-        //     hash[c]++;
-        // }
-        // int count = n;
-        //     for (int j = i; j < m; j++) {
-        //         if (hash[s[j]] > 0) {
-        //             count--;
-        //         }
-        //         hash[s[j]]--;
-        //         if (count == 0) {
-        //             if (j - i + 1 < minlen) 
-        //                 {minlen = j - i + 1;
-        //                 sindex = i;
-                        
-        //                 }
-        //                 break;
-        //         }
-        //     }
-        // }
-        // return sindex==-1?"": s.substr(sindex, minlen);
-        //t.c O(n^2)
-        // s.c:O(1) hash function is cosntant;
-        //optimal solution
-        int i=0;
-        int j=0;
-        int m = s.length();
-        int n = t.length();
-        int minlen = INT_MAX;
-        int sindex = -1;
+        int m=s.length();int n=t.length();
+        int i=0,j=0;
+        vector<int>hash(256,0);
         int count=n;
-     vector<int> hash(256, 0);
-        for (auto& c : t) {
+        int sindex=-1;
+        int minL=INT_MAX;
+        for(auto &c:t){
             hash[c]++;
         }
         while(j<m){
-            //expand window
- if (hash[s[j]] > 0) {
-                    count--;
-                }
-                hash[s[j]]--;
-                //if tb use krenge jab ek answer exist kr rha ho pure substring main aur while tab jab multiple answer exist kr rha ho mtlb min aur max diya ho tb to while use krenge kyunki tab hume min dusra window bhi  mil skta hai 
-              while (count == 0) {
-                    if (j - i + 1 < minlen) 
-                        {minlen = j - i + 1;
-                        sindex = i;
-                        }
-        
-        //shrink window
-        hash[s[i]]++;
-        if(hash[s[i]]>0){
-            count++;
+if(hash[s[j]]>0){
+    count--;
+}
+hash[s[j]]--;
+while(count==0){
+    if(j-i+1<minL){
+        minL=j-i+1;
+        sindex=i;
+    }
+    hash[s[i]]++;
+    if(hash[s[i]]>0){
+        count++;
+    }
+    i++;
+}
+j++;
         }
-        i++;
-                }
-                j++;
-        }
-        return sindex==-1?"": s.substr(sindex, minlen);
+        return sindex==-1?"":s.substr(sindex,minL);
     }
 };
