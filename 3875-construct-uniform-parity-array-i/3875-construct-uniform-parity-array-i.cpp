27455b374687fb49fc8f1aaa87714261c35f3810
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-09-02 07:20:01 +0530

    Time: 0 ms (100%), Space: 30.2 MB (94.95%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+       //  int n=nums1.size();
+       // int oddno=0;
+       //  int evenno=0;
+       //  for(auto x:nums1){
+       //      if(x&1){
+       //          oddno++;
+       //      } 
+       //      else
+       //          evenno++;
+       //  }
+       //  if( oddno==1 && evenno>1){
+       //      return false;
+       //  }
+       
+        
+
+        
+        
+                
+          
+
+        return true;
+        
+        
+    }
+};
