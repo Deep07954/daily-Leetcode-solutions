a19d7083226c0f99a491dec098dfa2078167a5bb
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-09-04 10:37:54 +0530

    Time: 0 ms (100%), Space: 31.1 MB (56.73%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -3,18 +3,33 @@ public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n=nums.size();
         
-        int score=-1;
+        // int score=-1;
        
-        for(int i=0;i<n;i++){
-             int maxelem=INT_MIN;
+        // for(int i=0;i<n;i++){
+        //      int maxelem=INT_MIN;
+        // int minelem=INT_MAX;
+        //     for(int j=0;j<=i;j++){
+        //     maxelem=max(maxelem,nums[j]); }
+        //     for(int p=i;p<n;p++){
+        //         minelem=min(minelem,nums[p]);
+        //     }
+        //     score=maxelem-minelem;
+        //     if(score<=k){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
         int minelem=INT_MAX;
-            for(int j=0;j<=i;j++){
-            maxelem=max(maxelem,nums[j]); }
-            for(int p=i;p<n;p++){
-                minelem=min(minelem,nums[p]);
-            }
-            score=maxelem-minelem;
-            if(score<=k){
+        vector<int>minidx(n);
+        for(int i=n-1;i>=0;i--){
+            minelem=min(minelem,nums[i]);
+            minidx[i]=minelem;
+        }
+        int maxelem=INT_MIN;
+        for(int i=0;i<n;i++){
+            maxelem=max(maxelem,nums[i]);
+            if(maxelem-minidx[i]<=k){
                 return i;
             }
         }
