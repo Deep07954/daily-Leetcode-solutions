46ce9fa4d303928893795a6c8b9a51c437f00228
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-09-20 12:09:43 +0530

    Time: 0 ms (100%), Space: 10.4 MB (85.22%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int req=tickets[k];
+        int time=0;
+        for(int i=0;i<tickets.size();i++){
+            if(i<=k){
+                time+=min(tickets[i],tickets[k]);
+            }
+            if(i>k){
+                time+=min(tickets[i],req-1);
+            }
+        }
+        return time;
+    }
+};
