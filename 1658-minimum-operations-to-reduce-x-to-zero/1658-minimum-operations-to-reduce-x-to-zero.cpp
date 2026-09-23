3c5bd7bd8d8e5bae3af264672a57820ee6635820
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-09-23 15:22:05 +0530

    Time: 154 ms (20.69%), Space: 155.9 MB (16.69%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,61 +1,28 @@
 class Solution {
 public:
-// int minm;
-// void solve(vector<int>&nums,int x,int count,int i,int j){
-//     if(x==0){
-// minm=min(minm,count);
-// return;
-//     }
-//     else if(x<0||i>j||count>minm){
-//         return;
-//     }
-// else {
-//    solve(nums,x-nums[i],count+1,i+1,j);
-// solve(nums,x-nums[j],count+1,i,j-1);}
-// }
-    int minOperations(vector<int>& nums, int x) {
-        int n=nums.size();
-  
-    //3 case possible hai left se minm ya right se ya fir dono side se
-//concept same as subarrays um k dhundhan hai aisa subarray
-int minm=INT_MIN;
-// int count=0;
-// int i=0;
-// int j=n-1;
-
-// solve(nums,x,0,0,n-1);
-// if(minm==INT_MAX){
-//     return -1;
-// }
-// return minm;
-int total=0;
-for(auto num:nums){
-total+=num;
-}
-int target=total-x;
-   int i=0;
-   int j=0;
-   
-   if(target==0){
-    return n;
-   }
-   if(target<0){
-    return -1;
-   }
-   int sum=0;
-   while(j<n){
-    
-sum+=nums[j];
-while(sum>target && i<=j){
-    sum-=nums[i];
-    i++;
-}
-if(sum==target){
-    minm=max(minm,j-i+1);
-}
-j++;
-   }
-   
-return minm==INT_MIN?-1:n-minm;
+    int minOperations(vector<int>& arr, int k) {
+         int n=arr.size();
+         int total=0;
+         for(auto num:arr){total+=num;} 
+         int target=total-k;
+         if(target<0)return -1;
+         if(target==0){
+            return n;
+         }
+      unordered_map<int,int>mp;
+      mp[0]=-1;
+     int maxL=INT_MIN;
+    int curr=0;
+      for(int i=0;i<n;i++){
+          curr+=arr[i];
+          if(mp.find(curr-target)!=mp.end()){
+              maxL=max(maxL,i-mp[curr-target]);
+          }
+           if(mp.find(curr)==mp.end()){
+              mp[curr]=i;
+          }
+      }
+      return maxL==INT_MIN?-1:n-maxL;
+     
     }
 };
