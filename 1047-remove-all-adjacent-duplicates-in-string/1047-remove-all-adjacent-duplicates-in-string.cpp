e12a9f01ba130aa98dc4565b8e9228cc882c24e6
Deep07954 <deepakk.ug23.ec@nitp.ac.in>
2026-09-26 19:18:36 +0530

    Time: 3 ms (82.54%), Space: 14.1 MB (58.98%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -3,25 +3,20 @@ public:
     string removeDuplicates(string s) {
         int n=s.length();
         stack<char>st;
-        st.push(s[0]);
-        int i=1;
-        while(i<n){
-            if(st.empty()|| st.top()!=s[i]){
-    st.push(s[i]);
-  
-}else {
-    st.pop();
-}
-i++;
-
-
+        
+        for(int i=0;i<n;i++){
+            if(!st.empty() && st.top()==s[i]){
+                st.pop();
+            }
+            else{
+            st.push(s[i]);}
         }
-        string res="";
+        string res;
         while(!st.empty()){
-res+=st.top();
-st.pop();
+        res+=st.top();
+        st.pop();
         }
         reverse(res.begin(),res.end());
         return res;
-            }
+    }
 };
